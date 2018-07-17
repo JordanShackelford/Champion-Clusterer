3226ad007fa6782b14b138b2382ddaf6f2e1a2de
Jordan Shackelford <jordanshackelford2@gmail.com>
2018-07-17 14:18:50 -0500

    Signed-off-by: Jordan Shackelford <jordanshackelford2@gmail.com>

---
+++ getData.py
@@ -0,0 +1,7 @@
+import requests
+
+apiKey = input()
+print(apiKey)
+
+#response = requests.get("http://api.champion.gg/v2/champions?elo=SILVER&champData=kda,damage,averageGames,totalHeal,killingSpree,minions,gold,positions,normalized,groupedWins,trinkets,runes,firstitems,summoners,skills,finalitems,masteries,maxMins,matchups&limit=200&api_key=<API KEY>")
+#print(response)
