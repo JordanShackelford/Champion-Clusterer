d44569d5e80727a44e63b411e73109a7480672eb
Jordan Shackelford <jordanshackelford2@gmail.com>
2018-07-18 21:00:01 -0500

    Successfully queried api and got a JSON object back in response
    Signed-off-by: Jordan Shackelford <jordanshackelford2@gmail.com>

--- getData.py
+++ getData.py
@@ -1,7 +1,14 @@
 import requests
+import json
 
-apiKey = input()
-print(apiKey)
+MYAPIKEY = input()
 
-#response = requests.get("http://api.champion.gg/v2/champions?elo=SILVER&champData=kda,damage,averageGames,totalHeal,killingSpree,minions,gold,positions,normalized,groupedWins,trinkets,runes,firstitems,summoners,skills,finalitems,masteries,maxMins,matchups&limit=200&api_key=<API KEY>")
-#print(response)
+response = requests.get("http://api.champion.gg/v2/champions?elo=SILVER&api_key=" + MYAPIKEY)
+
+# printing response instead of response.content will not display the data--------
+content = response.content
+print(content) 
+# bytes object needs to be decoded to a string to be used as a JSON object
+jcontent = json.loads(content.decode('utf8'))
+print(jcontent)
+#--------------------------------------------------------------------------------
