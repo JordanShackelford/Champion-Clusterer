649830f2db53f0adb6ebd82cd7b4bd05d134f53b
Jordan Shackelford <jordanshackelford2@gmail.com>
2018-07-21 19:30:46 -0500

    Small improvements, got rid of redundant button, made window non-resizable, added checkboxes
    Signed-off-by: Jordan Shackelford <jordanshackelford2@gmail.com>

--- getData.py
+++ getData.py
@@ -5,28 +5,46 @@ from tkinter import *
 def saveAPIKey():
 	myAPIKey = e1.get()
 	print("Your API key has been saved as " + myAPIKey)
-
+	sendQueryAndPrintResponse()
+	
+def sendQueryAndPrintResponse():
+	response = requests.get("http://api.champion.gg/v2/champions/1&api_key=" + myAPIKey)
+	content = response.content
+	print(content)
+	#jcontent = json.loads(content.decode('utf8'))
+	#print(jcontent)
+	
 master = Tk()
+master.resizable(False,False)
 myAPIKey = ""
 Label(master, text="Your API Key: ").grid(row=0)
 e1 = Entry(master)
 e1.grid(row=0, column=1)
-b = Button(master, text="SUBMIT", command=saveAPIKey)
+b = Button(master, text="Get Data", command = saveAPIKey)
 b.grid(row=0, column=2)
+cbvar1 = IntVar()
+Checkbutton(master, text="Silver", variable=cbvar1).grid(row=2, column=0)
+cbvar2 = IntVar()
+Checkbutton(master, text="Gold", variable=cbvar2).grid(row=2, column=1)
+cbvar3 = IntVar()
+Checkbutton(master, text="Platinum", variable=cbvar3).grid(row=2, column=2)
+cbvar4 = IntVar()
+Checkbutton(master, text="Diamond", variable=cbvar4).grid(row=2, column=3)
 mainloop( )
 
 
 
-'''
-MYAPIKEY = input()
 
-response = requests.get("api.champion.gg/v2/champions/1=" + MYAPIKEY)
+
+
+'''
+
+
 
 # printing response instead of response.content will not display the data--------
-content = response.content
+
 print(content) 
 # bytes object needs to be decoded to a string to be used as a JSON object
-jcontent = json.loads(content.decode('utf8'))
-print(jcontent)
+
 #--------------------------------------------------------------------------------
 '''
