6e16814ebe405f1e74a98d12028ff331562ae942
Jordan Shackelford <jordanshackelford2@gmail.com>
2018-07-23 21:05:39 -0500

    Added slider bar to select number of clusters to generate, changed the way API key is handled
    Signed-off-by: Jordan Shackelford <jordanshackelford2@gmail.com>

--- getData.py
+++ getData.py
@@ -5,29 +5,32 @@ from tkinter import *
 def saveAPIKey():
 	myAPIKey = e1.get()
 	print("Your API key has been saved as " + myAPIKey)
-	sendQueryAndPrintResponse()
+	sendQueryAndPrintResponse(myAPIKey)
 	
-def sendQueryAndPrintResponse():
-	response = requests.get("http://api.champion.gg/v2/champions/1&api_key=" + myAPIKey)
+def sendQueryAndPrintResponse(APIKey):
+	response = requests.get("http://api.champion.gg/v2/champions?&api_key=" + APIKey)
 	content = response.content
-	print(content)
-	#jcontent = json.loads(content.decode('utf8'))
+	#print(content)
+	jcontent = json.loads(content.decode('utf8'))
 	#print(jcontent)
+	
+	print(len(jcontent))
 	
 master = Tk()
 master.resizable(False,False)
-myAPIKey = ""
 Label(master, text="Your API Key: ").grid(row=0)
 e1 = Entry(master)
 e1.grid(row=0, column=1)
 b = Button(master, text="Get Data", command = saveAPIKey)
-b.grid(row=0, column=2)
+b.grid(row=0, column=3)
 
 eloVar = IntVar() #all the radio buttons are tied to this variable
-Radiobutton(master, text="Silver", variable=eloVar, value=1).grid(row=1,column=0)
-Radiobutton(master, text="Gold", variable=eloVar, value=2).grid(row=1,column=1)
-Radiobutton(master, text="Platinum", variable=eloVar, value=3).grid(row=1,column=2)
-Radiobutton(master, text="Diamond", variable=eloVar, value=4).grid(row=1,column=3)
+Radiobutton(master, text="Silver", variable=eloVar, value=1, indicatoron=0).grid(row=1,column=0)
+Radiobutton(master, text="Gold", variable=eloVar, value=2, indicatoron=0).grid(row=1,column=1)
+Radiobutton(master, text="Platinum", variable=eloVar, value=3, indicatoron=0).grid(row=1,column=2)
+Radiobutton(master, text="Diamond", variable=eloVar, value=4, indicatoron=0).grid(row=1,column=3)
+
+w = Scale(master, label="# of Clusters:", from_= 0, to = 100, orient=HORIZONTAL).grid()
 mainloop( )
 
 
