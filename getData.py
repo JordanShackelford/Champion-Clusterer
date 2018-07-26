0d53280ee9426fc953f55e4d9774eef157c7dc25
Jordan Shackelford <jordanshackelford2@gmail.com>
2018-07-25 22:43:01 -0500

    removed radio buttons just adds unnecessary complexity. will either make it default to platinum or just use all data
    Signed-off-by: Jordan Shackelford <jordanshackelford2@gmail.com>

--- getData.py
+++ getData.py
@@ -32,14 +32,10 @@ e1.grid(row=0, column=1)
 b = Button(master, text="Get Data", command = saveAPIKey)
 b.grid(row=0, column=3)
 
-eloVar = IntVar() #all the radio buttons are tied to this variable
-Radiobutton(master, text="Silver", variable=eloVar, value=1, indicatoron=0).grid(row=1,column=0)
-Radiobutton(master, text="Gold", variable=eloVar, value=2, indicatoron=0).grid(row=1,column=1)
-Radiobutton(master, text="Platinum", variable=eloVar, value=3, indicatoron=0).grid(row=1,column=2)
-Radiobutton(master, text="Diamond", variable=eloVar, value=4, indicatoron=0).grid(row=1,column=3)
-
 w = Scale(master, label="# of Clusters:", from_= 2, to = 20, orient=HORIZONTAL).grid()
 mainloop( )
+
+#github won't push this file...
 
 
 
